258a50b4d8d9365c8bbde28186f0390bb0925090
apodes <aaronpodell@gmail.com>
2013-07-31 13:23:41 -0400

    initial commit - foundation already present

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+	def home
+	end
+
+	def staff
+	end
+
+	def insurance
+	end
+
+	def questions
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrPavelObgyn::Application.config.secret_token = '95ace98588ae6a3101aa7e16dd830bc1efc54ba1dd590fcad862ab7e034801df3dfdce0bdcaa05a383db4ddf6f0a909e3adea947a9b366fc1f41d70c904ca598'
